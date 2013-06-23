bc4db0eec6c8fd10affaef7d1fe2d97dcfca1c5e
jkroso <jkroso@gmail.com>
2013-06-23 15:02:40 +1200

    test

--- test/read-slice.test.js
+++ test/read-slice.test.js
@@ -3,9 +3,9 @@ var chai = require('./chai')
   , slice = require('..')
 
 describe('read-slice', function(){
-	it('should return a string', function(done){
-		slice(__filename, 1, 29).then(function(str){
-			str.should.equal("var chai = require('./chai')")
+	it('should return a buffer', function(done){
+		slice(__filename, 1, 29).then(function(buf){
+			buf.should.eql(new Buffer("var chai = require('./chai')"))
 		}).node(done)
 	})
 })
